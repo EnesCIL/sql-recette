67ae5953180bc73bba0ed13711d29edebe53c731
EnesCIL <enescil2003@hotmail.com>
2025-11-12 17:02:47 +0100

    plusieurs exo fini

--- sql/recetteexo.sql
+++ sql/recetteexo.sql
@@ -9,11 +9,14 @@ INNER JOIN categorie c on r.id_categorie = c.id_categorie
 ORDER BY c.id_categorie DESC
 
  2-
- SELECT r.nom, r.tempsPreparation, c.nomCategorie , inomIngredient
- FROM recette r
- INNER JOIN categorie c on r.id_categorie = c.id_categorie 
- INNER JOIN ingredient i ON i.id_ingredient= r.id_recette
- ORDER BY c.id_categorie DESC
+ SELECT r.nom, r.tempsPreparation, c.nomCategorie , COUNT(ri.id_ingredient) AS nbIngredients
+ FROM categorie c
+ INNER JOIN recette r ON r.id_categorie = c.id_categorie
+ INNER JOIN recette_ingredient ri ON ri.id_recette = r.id_recette
+ GROUP BY r.nom, r.tempsPreparation, c.nomCategorie 
+
+
+
 
 3-
 -- selectionne le nom depuis recette
@@ -22,8 +25,9 @@ SELECT nom FROM recette
 WHERE tempsPreparation >= 30
 
 4-
-
-SELECT "salade"  FROM recette
+SELECT nom
+FROM recette
+WHERE nom LIKE '%salade%'
 
 5-
 -- inserer une nouvelle ligne 
@@ -49,36 +53,48 @@ WHERE id_recette = 2
 
 8-
 
-SELECT SUM(prix) AS prix_total
-FROM ingredient
-WHERE id_ingredient = 5
+SELECT SUM(ri.quantite * i.prix) AS prix_total
+FROM recette r 
+INNER JOIN recette_ingredient ri ON ri.id_recette = r.id_recette
+INNER JOIN ingredient i ON i.id_ingredient = ri.id_ingredient
+WHERE  r.id_recette = 10
 
 9-
-SELECT nomIngredient rn, quantite q, prix p
-FROM recipe r
-INNER JOIN recette_ingredient ri ON r.id_recipe = ri.id_recipe
+SELECT nomIngredient , quantite , prix 
+FROM recette r
+INNER JOIN recette_ingredient ri ON r.id_recette = ri.id_recette
 INNER JOIN ingredient i ON ri.id_ingredient = i.id_ingredient
-WHERE r.id_recipe = 5;
+WHERE r.id_recette = 5;
+
 
 10-
 INSERT INTO ingredient(nomIngredient, unite, prix)
 VALUES ("Poivre", "cuillère à café", 2.5);
 
-SELECT * FROM ingredient WHERE ingredient_name = "Poivre";
 11-
+
 
 UPDATE ingredient
 SET prix = 1
-WHERE id_ingredient = 12;
+WHERE id_ingredient = 10;
 
-SELECT * FROM ingredient WHERE id_ingredient = 12;
+
 12-
-SELECT  COUNT(nom)
+SELECT  COUNT(r.id_recette) AS nbRecette, c.nomCategorie
+FROM recette r
+INNER JOIN categorie c on r.id_categorie = c.id_categorie
+GROUP BY c.id_categorie
+
 
 
 
 13-
-SELECT "poulet"  FROM ingredient
+SELECT r.nom 
+FROM recette r
+INNER JOIN recette_ingredient ri ON ri.id_recette = r.id_recette
+INNER JOIN ingredient i ON i.id_ingredient = ri.id_ingredient
+WHERE i.nomIngredient LIKE '%poulet%'
+
 
 
 14-
@@ -86,17 +102,36 @@ SELECT nom, tempsPreparation
 UPDATE recette
 Set nom = tempsPreparation - 5;
 
-
+UPDATE recette r
+SET r.tempsPreparation - 5;
 15-
-SELECT nom 
+SELECT r.nom
+FROM recette r
+WHERE r.id_recette NOT IN (
+	SELECT ri.id_recette
+	FROM recette_ingredient ri
+	INNER JOIN ingredient i ON ri.id_ingredient = i.id_ingredient
+	WHERE ri.id_recette = r.id_recette
+	AND i.prix > 2
+);
 
 16-
-SELECT r.nom, r.preparation_time
+SELECT r.nom, r.tempsPreparation
 FROM recette r
-WHERE  MIN(preparation_time)
+WHERE r.tempsPreparation = (
+	SELECT  MIN(r.tempsPreparation) 
+	FROM recette r
+);
   
 17-
-
+SELECT r.nom
+FROM recette r
+WHERE r.id_recette NOT IN (
+	SELECT ri.id_recette
+	FROM recette_ingredient ri
+	INNER JOIN ingredient i ON ri.id_ingredient = i.id_ingredient
+	WHERE ri.id_recette = r.id_recette
+);
 18-
 
 19-
